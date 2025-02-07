236eead37d307592638f627d3a4193332d8f8762
Amr Tamer <114962896+AmrTamerDev@users.noreply.github.com>
2025-02-08 01:59:37 +0200

    Q1) Write a class named Calculator that contains a method named Add. Overload the Add method to:

---
+++ Assignment4/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class Calculator
+    {
+        public static int Sum(int Num1, int Num2)
+        {
+            return Num1 + Num2;
+        }
+
+        public static int Sum(int Num1, int Num2, int Num3)
+        {
+            return Num1 + Num2 + Num3;
+        }
+
+        public static double Sum(double Num1, double Num2)
+        {
+            return Num1 + Num2;
+        }
+    }
+}
--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            int Result1 = Calculator.Sum(5, 6);
+            int Result2 = Calculator.Sum(10, 6);
+            double Result3 = Calculator.Sum(5.5, 6.6);
+
+            Console.WriteLine(Result1);
+            Console.WriteLine(Result2);
+            Console.WriteLine(Result3);
         }
     }
 }
