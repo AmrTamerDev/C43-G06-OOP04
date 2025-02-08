fdb5d4c67a186d5624673c03aab1373f89b6e60c
Amr Tamer <114962896+AmrTamerDev@users.noreply.github.com>
2025-02-08 02:48:13 +0200

    Q3) Define a class Complex Number that represents a complex number with real and imaginary parts.

---
+++ Assignment4/ComplexNumber.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class ComplexNumber
+    {
+        #region Attributes
+        private int real;
+        private int imaginary;
+        #endregion
+
+        #region Properties
+        public int Real
+        {
+            get { return real; }
+            set { real = value; }
+
+        }
+
+        public int Imaginary
+        {
+            get { return imaginary; }
+            set { imaginary = value; }
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{real} + {imaginary}i";
+        }
+
+        public static ComplexNumber operator + (ComplexNumber Num1C, ComplexNumber Num2C)
+        {
+            return new ComplexNumber()
+            {
+                real = (Num1C?.real ?? 0) + (Num2C?.real ?? 0),
+                imaginary = (Num1C?.imaginary ?? 0) + (Num2C?.Imaginary ?? 0)
+            };
+        }
+
+        public static ComplexNumber operator -(ComplexNumber Num1C, ComplexNumber Num2C)
+        {
+            return new ComplexNumber()
+            {
+                real = (Num1C?.real ?? 0) - (Num2C?.real ?? 0),
+                imaginary = (Num1C?.imaginary ?? 0) - (Num2C?.Imaginary ?? 0)
+            };
+        }
+        #endregion
+
+
+    }
+}
--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -12,14 +12,31 @@
             //Console.WriteLine(Result2);
             //Console.WriteLine(Result3);
 
-            Rectangle rectangle1 = new Rectangle();
-            Console.WriteLine(rectangle1);
+            //Rectangle rectangle1 = new Rectangle();
+            //Console.WriteLine(rectangle1);
 
-            Rectangle rectangle2 = new Rectangle(1,2);
-            Console.WriteLine(rectangle2);
+            //Rectangle rectangle2 = new Rectangle(1,2);
+            //Console.WriteLine(rectangle2);
             
-            Rectangle rectangle3 = new Rectangle(1);
-            Console.WriteLine(rectangle3);
+            //Rectangle rectangle3 = new Rectangle(1);
+            //Console.WriteLine(rectangle3);
+
+            ComplexNumber complexNumber1 = new ComplexNumber();
+            ComplexNumber complexNumber2 = new ComplexNumber();
+            ComplexNumber complexNumber3 = new ComplexNumber();
+            ComplexNumber complexNumber4 = new ComplexNumber();
+
+            complexNumber1.Real = 10;
+            complexNumber2.Real = 20;
+
+            complexNumber1.Imaginary = 30;
+            complexNumber2.Imaginary = 40;
+
+            complexNumber3 = complexNumber1 + complexNumber2;
+            complexNumber4 = complexNumber1 - complexNumber2;
+
+            Console.WriteLine(complexNumber3);
+            Console.WriteLine(complexNumber4);
         }
     }
 }
