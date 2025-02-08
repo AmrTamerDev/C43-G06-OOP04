d7c3baaa53a5cd509bcb563c865a0a1e01c44155
Amr Tamer <114962896+AmrTamerDev@users.noreply.github.com>
2025-02-08 02:24:51 +0200

    Q2) Create a class named Rectangle with the following constructors:

--- Assignment4/Program.cs
+++ Assignment4/Program.cs
@@ -4,13 +4,22 @@
     {
         static void Main(string[] args)
         {
-            int Result1 = Calculator.Sum(5, 6);
-            int Result2 = Calculator.Sum(10, 6);
-            double Result3 = Calculator.Sum(5.5, 6.6);
+            //int Result1 = Calculator.Sum(5, 6);
+            //int Result2 = Calculator.Sum(10, 6);
+            //double Result3 = Calculator.Sum(5.5, 6.6);
 
-            Console.WriteLine(Result1);
-            Console.WriteLine(Result2);
-            Console.WriteLine(Result3);
+            //Console.WriteLine(Result1);
+            //Console.WriteLine(Result2);
+            //Console.WriteLine(Result3);
+
+            Rectangle rectangle1 = new Rectangle();
+            Console.WriteLine(rectangle1);
+
+            Rectangle rectangle2 = new Rectangle(1,2);
+            Console.WriteLine(rectangle2);
+            
+            Rectangle rectangle3 = new Rectangle(1);
+            Console.WriteLine(rectangle3);
         }
     }
 }
---
+++ Assignment4/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    public class Rectangle
+    {
+        #region Attributes
+        private int width;
+        private int height;
+
+        #endregion
+
+        #region Properties
+        public int Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+
+        public int Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        #endregion
+
+
+        #region Constructors
+        public Rectangle()
+        {
+            height = 0;
+            width = 0;
+        }
+
+        public Rectangle(int Width, int Height)
+        {
+            width = Width;
+            height = Height;
+        }
+
+        public Rectangle(int Value)
+        {
+            width = height = Value;
+        }
+        #endregion
+
+        public override string ToString()
+        {
+            return $"Wight = {width}\nHeight{height}";
+        }
+    }
+}
